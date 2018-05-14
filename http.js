d2eeb7906830b42f4612e8734606e2d4a027e1b6
unknown <906454@chant.local>
2018-05-14 12:28:13 +0800

    http

--- http.js
+++ http.js
@@ -1,6 +1,11 @@
 var http = require("http");
-http.createServer(function (require, response){
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('Hello World!\n');
+http.createServer(function (req,res){
+	res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
+	res.write(util.inspect(url.parse(req.url, true)));
+	var params = url.parse(req.url, true).query;
+	res.write('\nname=' + params.name);
+	res.write('\nage=' + params.age);
+	res.end();
+
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80');
